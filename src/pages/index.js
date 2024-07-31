343b60096b2aa906b3015d8982112fc4b331c54c
Alisina <99441567+imalisina@users.noreply.github.com>
2024-07-31 23:00:55 +0100

    added markdown functions

---
+++ src/pages/index.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import * as marked from 'marked'; // Updated import
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  const [markdown, setMarkdown] = useState(`# Welcome to Markdown Previewer!
+
+## This is a sub-heading
+
+[GitHub](https://github.com/)
+
+Inline \`code\`
+
+\`\`\`
+function helloWorld() {
+  console.log("Hello, world!");
+}
+\`\`\`
+
+- List item 1
+- List item 2
+
+> Blockquote
+
+![Image](https://via.placeholder.com/150)
+
+**Bold text**
+`);
+
+  useEffect(() => {
+    // Update the preview when the component mounts
+    setMarkdown(markdown);
+  }, []);
+
+  return (
+    <div id="markdown-previewer" className={styles.container}>
+      <textarea
+        id="editor"
+        className={styles.editor}
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+      <div
+        id="preview"
+        className={styles.preview}
+        dangerouslySetInnerHTML={{ __html: marked.parse(markdown) }} // Updated function call
+      />
+    </div>
+  );
+}
